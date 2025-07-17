031fe830aa89f56b7e872c162afc2a5c6198ba2c
Kaelem Chandra <kc@kaelem.net>
2025-07-17 07:57:21 +0000

    Add correct repo

--- vars.sh
+++ vars.sh
@@ -1,4 +1,4 @@
 USER_PREFIX="group"
 GROUP_COUNT=8
-REPO_URL=""
-REPO_DIR="$(basename https://github.com/thcorre/phnog2025-workshop)"
+REPO_URL="https://github.com/thcorre/idnog10-workshop"
+REPO_DIR="$(basename $REPO_URL)"
