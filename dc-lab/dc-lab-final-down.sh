7b13e75be89a68e4006f4545a4c9fcd5a4ba4498
Thomas Corre <correthomas@gmail.com>
2025-07-17 23:19:42 +0800

    Minor correction on scripts

--- dc-lab/dc-lab-final-down.sh
+++ dc-lab/dc-lab-final-down.sh
@@ -5,5 +5,5 @@ LAB_SUBDIR="dc-lab"
 for i in $(seq 1 $GROUP_COUNT);
 do
     USERNAME=$USER_PREFIX$i
-    sudo -u $USERNAME -H bash -c "source ~/.bashrc; clab des -c -t ~/$REPO_DIR/$LAB_SUBDIR/\*-final.clab.yml"
+    sudo -u $USERNAME -H bash -c "source ~/.bashrc; clab des -c -t ~/$REPO_DIR/$LAB_SUBDIR/\*final.clab.yml"
 done
--- dc-lab/dc-lab-init-2-up.sh
+++ dc-lab/dc-lab-init-2-up.sh
@@ -5,5 +5,5 @@ LAB_SUBDIR="dc-lab"
 for i in $(seq 1 $GROUP_COUNT);
 do
     USERNAME=$USER_PREFIX$i
-    sudo -u $USERNAME -H bash -c "source ~/.bashrc; clab dep -c -t ~/$REPO_DIR/$LAB_SUBDIR/\*-init-2.clab.yml"
+    sudo -u $USERNAME -H bash -c "source ~/.bashrc; clab dep -c -t ~/$REPO_DIR/$LAB_SUBDIR/\*init-2.clab.yml"
 done
