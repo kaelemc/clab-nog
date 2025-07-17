5e529d1f1f08e34e3a8b65aca4d919bbdb4619e3
Thomas Corre <correthomas@gmail.com>
2025-07-17 23:06:18 +0800

    scripts update for different config states

--- dc-lab/dc-lab-down.sh
+++
@@ -1,9 +0,0 @@
-source ../vars.sh
-
-LAB_SUBDIR="dc-lab"
-
-for i in $(seq 1 $GROUP_COUNT);
-do
-    USERNAME=$USER_PREFIX$i
-    sudo -u $USERNAME -H bash -c "source ~/.bashrc; clab des -c -t ~/$REPO_DIR/$LAB_SUBDIR"
-done
