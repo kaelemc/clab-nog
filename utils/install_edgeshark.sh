5434de4781da211ae6afc0d3a569e36ff2a81144
Kaelem Chandra <kc@kaelem.net>
2025-07-17 08:01:53 +0000

    Format

--- utils/install_edgeshark.sh
+++ utils/install_edgeshark.sh
@@ -1,4 +1,2 @@
 # https://github.com/siemens/edgeshark
-wget -q --no-cache -O - \
-  https://github.com/siemens/edgeshark/raw/main/deployments/wget/docker-compose.yaml \
-  | DOCKER_DEFAULT_PLATFORM= docker compose -f - up
+wget -q --no-cache -O - https://github.com/siemens/edgeshark/raw/main/deployments/wget/docker-compose.yaml | DOCKER_DEFAULT_PLATFORM= docker compose -f - up -d
