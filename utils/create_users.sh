289c1555845f0eac0262c604fdb3bd355131ff97
Kaelem Chandra <kc@kaelem.net>
2025-07-18 05:20:52 +0000

    Update for 9 groups

--- utils/create_users.sh
+++ utils/create_users.sh
@@ -5,7 +5,7 @@ for i in $(seq 1 $GROUP_COUNT);
 do
   USER=$USER_PREFIX$i
   sudo useradd -m -s /bin/bash $USER
-  echo "$USER$i:$USER$i" | sudo chpasswd
+  echo "$USER:$USER" | sudo chpasswd
   # Set GROUP_ID on login
   sudo bash -c "echo 'export GROUP_ID=$i' >> /home/$USER/.bashrc"
   sudo chown $USER:$USER /home/$USER/.bashrc
