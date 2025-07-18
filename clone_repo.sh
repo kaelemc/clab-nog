3b838a1b965ede1fcaa65956d0480783d280e8c7
Kaelem Chandra <kc@kaelem.net>
2025-07-18 06:45:40 +0000

    Add comment for a 'force' clone -- ie. delete repodir then reclone

--- clone_repo.sh
+++ clone_repo.sh
@@ -3,5 +3,6 @@ source vars.sh
 for i in $(seq 1 $GROUP_COUNT);
 do
     USERNAME=$USER_PREFIX$i
+    # rm -rf /home/$USERNAME/$REPO_DIR
     sudo -u $USERNAME -H bash -c "cd ~; git clone $REPO_URL" 
 done
